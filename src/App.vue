153f53c6c3defa25f75ad9b9b5b1cb43297f6cac
AzKL42 <93662113+AzKL42@users.noreply.github.com>
2024-11-23 21:19:40 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <Toggle /> -->
+  <!-- <Menu /> -->
+  <router-link to="/Home">首页</router-link>
+  <router-link to="/Login">Toggle</router-link>
+  <router-view />
+</template>
+
+<script setup>
+import Toggle from './views/Toggle.vue';
+import Menu from './views/Home.vue';
+
+</script>
+
+<style >
+
+</style>
---
+++ src/layout/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+    <Header />
+    <!--    主体-->
+    <div style="display: flex">
+      <!--      侧边栏-->
+      <Aside />
+      <!--      内容区域-->
+      <router-view style="flex: 1"/>
+    </div>
+    <!-- <Main /> -->
+  </template>
+  
+  <script>
+  
+  import Header from "../components/Header.vue";
+  import Aside from "../components/Aside.vue";
+  // import Main from "../components/Main.vue";
+  export default {
+    name: "Layout",
+    components:{
+      Header,
+      Aside,
+      // Main
+    }
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
