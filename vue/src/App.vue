0bc1bb15665e8de55a7cf8243af1ceba5ba04e07
AzKL42 <93662113+AzKL42@users.noreply.github.com>
2024-11-24 17:07:36 +0800

    注释登录部分，为了在没有路由的情况下显示首页

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <!-- <Toggle /> -->
-  <!-- <Menu /> -->
-  <router-link to="/Home">首页</router-link>
+  <!-- <Home /> -->
+  <!-- <router-link to="/Home">首页</router-link>
   <router-link to="/Login">Toggle</router-link>
-  <router-view />
+  <router-view /> -->
+  <Layout />
 </template>
 
 <script setup>
 import Toggle from './views/Toggle.vue';
-import Menu from './views/Home.vue';
+import Home from './views/Home.vue';
+import Layout from './layout/Layout.vue';
 
 </script>
 
--- vue/src/main.js
+++ vue/src/main.js
@@ -4,7 +4,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import './assets/global.css'; // 引入全局样式
-import router from './router/index.js'
+// import router from './router/index.js'
 
 const app = createApp(App)
 
@@ -12,6 +12,6 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 
-app.use(router)
+// app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
