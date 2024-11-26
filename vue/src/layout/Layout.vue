4fb55a9df331c30ce7cf0ac1b2d312a4883e7210
AzKL42 <93662113+AzKL42@users.noreply.github.com>
2024-11-26 21:41:33 +0800

    修改了布局

--- vue/src/layout/Layout.vue
+++ vue/src/layout/Layout.vue
@@ -4,8 +4,18 @@
   <div style="display: flex">
     <!--      侧边栏-->
     <Aside />
-    <!--      内容区域-->
-    <!-- <router-view style="flex: 1"/> -->
+    <!--      主内容区域-->
+    <div>
+      <!-- 静态模块 -->
+      <template v-if="$route.name === 'Home'">
+        <Carousel />
+        <!-- <BookRecommendation />
+        <QuickService />
+        <Announcement /> -->
+      </template>
+      <!-- 动态模块 -->
+      <router-view style="flex: 1"/> <!--style="flex: 1" 表示这个元素会占据剩余的所有可用空间-->
+    </div>
   </div>
 </template>
   
@@ -13,11 +23,17 @@
 
 import Header from "../components/Header.vue";
 import Aside from "../components/Aside.vue";
+import Carousel from "../components/Carousel.vue";
+// import BookRecommendation from "../components/BookRecommendation.vue";
+// import QuickService from "../components/QuickService.vue";
+// import Announcement from "../components/Announcement.vue";
+
 export default {
   name: "Layout",
   components:{
     Header,
-    Aside
+    Aside,
+    Carousel,
   }
 }
 </script>
