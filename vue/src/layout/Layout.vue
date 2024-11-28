8c247f984469c36be86b8d4834bdd2ef63c48511
AzKL42 <93662113+AzKL42@users.noreply.github.com>
2024-11-28 16:08:50 +0800

    布局调整，增加了首页的静态组件

--- vue/src/layout/Layout.vue
+++ vue/src/layout/Layout.vue
@@ -5,13 +5,16 @@
     <!--      侧边栏-->
     <Aside />
     <!--      主内容区域-->
-    <div>
+    <div style="display: flex; flex-direction: column; width: 100%;">
       <!-- 静态模块 -->
       <template v-if="$route.name === 'Home'">
-        <Carousel />
-        <!-- <BookRecommendation />
-        <QuickService />
-        <Announcement /> -->
+        <div class="module-row">
+          <Carousel />
+          <Announcement />
+        </div>
+        <div class="module-row">
+          <BookRecommendation />
+        </div>
       </template>
       <!-- 动态模块 -->
       <router-view style="flex: 1"/> <!--style="flex: 1" 表示这个元素会占据剩余的所有可用空间-->
@@ -24,9 +27,8 @@
 import Header from "../components/Header.vue";
 import Aside from "../components/Aside.vue";
 import Carousel from "../components/Carousel.vue";
-// import BookRecommendation from "../components/BookRecommendation.vue";
-// import QuickService from "../components/QuickService.vue";
-// import Announcement from "../components/Announcement.vue";
+import BookRecommendation from "../components/BookRecommendation.vue";
+import Announcement from "../components/Announcement.vue";
 
 export default {
   name: "Layout",
@@ -34,10 +36,25 @@ export default {
     Header,
     Aside,
     Carousel,
+    BookRecommendation,
+    Announcement
   }
 }
 </script>
 
 <style scoped>
+
+.module-row {
+  display: flex;
+  justify-content: space-between; /* 模块之间有间距 */
+  width: 100%;
+  margin-bottom: 20px; /* 模块行之间的间距 */
+  margin-top: 10px; /* 模块行与顶部的间距 */
+}
+
+.module-row > * {
+  flex: 1; /* 每个模块占据相等的空间 */
+  margin: 0 10px; /* 模块之间的间距 */
+}
 
 </style>
